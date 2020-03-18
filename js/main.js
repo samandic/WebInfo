967198107fd53198bb987582e6316aae8be90f2a
smandic <sasa.mandic@codecons.com>
2020-03-18 12:35:37 +0100

    COmmit 2

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var init = function () {
     console.log('init() called');
     $(".text1").html('XXXXXXXXXXXXXX');
     $(".mypanel").html('XXXXXXX');
+    parseJson2();
     document.addEventListener('visibilitychange', function() {
         if(document.hidden){
             // Something you want to do when hide or exit.
@@ -39,14 +40,42 @@ var init = function () {
 // window.onload can work without <body onload="">
 window.onload = init;
 
+function parseJson2() {
+	var reviver;
+    var hr = new XMLHttpRequest();
+    var url = "http://time.jsontest.com";
+    $(".mypanel").html("processing...1");
+    hr.open("GET", url, true);
+    
+    $(".mypanel").html("processing...2");
+    hr.setRequestHeader("Content-type", "script/json");
+    
+    $(".mypanel").html("processing...3");
+
+    hr.onreadystatechange = function() {
+        $(".mypanel").html(hr.status);
+        if(hr.status == 200) {
+
+        	$(".mypanel").html("processing...4");
+            var return_data = JSON.parse(  hr.responseText, reviver);
+            $(".text1").html('${data.date}');
+            //document.getElementById("status").innerHTML = return_data.citation[0].id;
+        }
+    }
+    //$(".mypanel").html("processing...5");
+    hr.send(null);
+    //$(".mypanel").html("processing...6");
+}
+
 function parseJson() {
 	var obj = JSON.parse('{ "name":"John", "age":30, "city":"New York"}');
 	document.getElementById('text1').innerHTML=obj.name + ", " + obj.age;
 	$(".text1").html('new text');
 	 $.getJSON('http://time.jsontest.com', function(data) {
-		 var text = 'Date: ${data.date}<br> Time: ${data.time}<br> Unix time: ${data.milliseconds_since_epoch}'   
+		 var text = 'Date: ${data.date}';
 	       
-			 $(".mypanel").html(text);
+		$(".mypanel").html(text);
+		 $(".text1").html('text2');
 	    });
 }
 
